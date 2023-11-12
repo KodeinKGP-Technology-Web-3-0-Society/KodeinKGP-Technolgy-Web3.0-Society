342b629f16bce58e5558ffdbc24af0fd51348a97
rampreetham <rampreethamkanchi@gmail.com>
2023-11-12 16:09:38 +0530

    added problem statements 2 and 3 to events page

--- frontend/src/components/EventsPage/ProblemStatement3.js
+++ frontend/src/components/EventsPage/ProblemStatement3.js
@@ -1,12 +1,3 @@
-// import React from 'react';
-
-// const ProblemStatement3 = ({id}) =>{
-//     return (
-//         <div className='problem3' style={{color:"white"}}>Problem3</div>
-//     )
-// }
-
-// export default ProblemStatement3;
 import React from "react";
 
 const ProblemStatement3 = () => {
@@ -65,48 +56,50 @@ const ProblemStatement3 = () => {
             <p className="problem_subheading">Resources Required</p>
             <br />
             <p className="problem_text">
-                Machine Learning Frameworks: Utilize machine learning frameworks
-                like TensorFlow or PyTorch for model development and training.
+                <span>1. Machine Learning Frameworks:</span> Utilize machine
+                learning frameworks like TensorFlow or PyTorch for model
+                development and training.
             </p>
             <p className="problem_text">
-                Natural Language Processing Tools: Employ NLP libraries such as
-                NLTK, spaCy, and Hugging Face Transformers for text
-                preprocessing, analysis, and model fine-tuning.
+                <span>2. Natural Language Processing Tools:</span> Employ NLP
+                libraries such as NLTK, spaCy, and Hugging Face Transformers for
+                text preprocessing, analysis, and model fine-tuning.
             </p>
             <p className="problem_text">
-                Hardware: Access to GPU or TPU resources for training and
-                deploying the AI model.
+                <span>3. Hardware:</span> Access to GPU or TPU resources for
+                training and deploying the AI model.
             </p>
             <p className="problem_text">
-                Data Storage and Management: Implement a secure and scalable
-                data storage system for user data.
+                <span>4. Data Storage and Management:</span> Implement a secure
+                and scalable data storage system for user data.
             </p>
             <p className="problem_text">
-                User Interface Development: Skills and tools for designing a
-                user-friendly interface.
+                <span>5. User Interface Development:</span> Skills and tools for
+                designing a user-friendly interface.
             </p>
             <p className="problem_text">
-                Privacy and Security Expertise: Knowledge and best practices for
-                ensuring data privacy and security.
+                <span>6. Privacy and Security Expertise:</span> Knowledge and
+                best practices for ensuring data privacy and security.
             </p>
+
             <br />
             <p className="problem_subheading">Considerations</p>
             <br />
             <p className="problem_text">
-                <span>The model's output should be:</span> high-quality,
-                coherent, and closely mimic the user's writing style.
+                The model's output should be high-quality, coherent, and closely
+                mimic the user's writing style.
             </p>
             <p className="problem_text">
-                <span>Compliance with privacy regulations is paramount.</span>{" "}
-                Users must be fully informed about data collection and usage.
+                Compliance with privacy regulations is paramount. Users must be
+                fully informed about data collection and usage.
             </p>
             <p className="problem_text">
-                <span>User consent and data anonymization are crucial</span> to
-                respect privacy.
+                User consent and data anonymization are crucial to respect
+                privacy.
             </p>
             <p className="problem_text">
-                <span>Ethical considerations</span> are important, particularly
-                regarding the potential to impersonate a user's writing style.
+                Ethical considerations are important, particularly regarding the
+                potential to impersonate a user's writing style.
             </p>
             <br />
             <p className="problem_subheading">Evaluation Criteria</p>
@@ -128,18 +121,17 @@ const ProblemStatement3 = () => {
 
             <br />
             <p className="problem_subheading">Deliverables</p>
+            <br />
             <p className="problem_text">
-                <span>
-                    1. A functional AI system that generates personalized
-                    content.
-                </span>
+                <span>1.</span> A functional AI system that generates
+                personalized content.
             </p>
             <p className="problem_text">
-                <span>2. Documentation:</span> Outlining the data handling,
-                model architecture, and privacy measures.
+                <span>2.</span> Documentation outlining the data handling, model
+                architecture, and privacy measures.
             </p>
             <p className="problem_text">
-                <span>3. A presentation:</span> Showcasing the system's features
+                <span>3. </span> A presentation showcasing the system's features
                 and capabilities.
             </p>
         </div>
