777bdf9950ed903a4b95749c003afd851395c591
sakethakella <63924046+sakethakella@users.noreply.github.com>
2024-06-09 18:28:36 +0530

    Update Home.js

--- src/components/BreadthRecommender/Home.js
+++ src/components/BreadthRecommender/Home.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const Home = () =>{
-    return <h1>Home</h1>;
+    return (
+    <div className="container">
+      <div className="welcome-box">
+        <div className='form1'>
+        <h1>WELCOME</h1>
+        <p>Please enter your roll number</p>
+        <form>
+          <input
+            type="text"
+            placeholder="XXXXXXXXX"
+          />
+          <br/>
+          <button type="submit">SUBMIT</button>
+        </form>
+        <div className="arrow"></div>
+          </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
