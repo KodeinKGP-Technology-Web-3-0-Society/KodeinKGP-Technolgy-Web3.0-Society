bd0c201b96c60b9348b964b795067ceef52a6741
Bikram Ghuku <bikramghuku05@gmail.com>
2024-06-12 11:27:59 +0530

    feat: added time selector element

---
+++ src/components/BreadthRecommender/Choices/TimeSel.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import './TimeSel.css'
+
+function TimeSel() {
+    const [availTimes ,setAvailTimes] = useState([])
+    
+    useEffect(()=>{
+        var localTimes = []
+        for(var i = 8; i < 12; i+=2){
+            localTimes.push(`${i} AM`)
+        }
+        for(var i = 1; i < 6; i+=2){
+            localTimes.push(`${i} PM`)
+        }
+        setAvailTimes(localTimes)
+    }, [])
+    
+    return (
+        <div className='ts-main'>
+            <div className="ts-opts">
+                <h1 className="ts-opts-title">SELECT TIMESLOT</h1>
+            </div>
+            <div className="ts-contents"></div>
+            <div className="ts-btn">
+                <div className="ts-cl-btn-wrap">
+                    <button className='ts-cl-btn'>
+                        Clear All
+                    </button>
+                </div>
+                <div className="ts-ap-btn-wrap">
+                    <button className='ts-ap-btn'>
+                        Apply
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TimeSel
