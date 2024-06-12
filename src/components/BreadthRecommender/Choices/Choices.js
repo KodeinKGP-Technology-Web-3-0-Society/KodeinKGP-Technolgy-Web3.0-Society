543837db7856878cb768139a7f14800343bd6584
Bikram Ghuku <bikramghuku05@gmail.com>
2024-06-12 11:43:19 +0530

    feat: ui update and logic to open close dialog box

--- src/components/BreadthRecommender/Choices/Choices.js
+++ src/components/BreadthRecommender/Choices/Choices.js
@@ -7,7 +7,7 @@ import GoalSel from './GoalSel';
 function Choices() {
     const [data, setData] = useState([]);
     const [goalVis, setGoalVis] = useState(false)
-    const [timeVis, settimeVis] = useState(false)
+    const [timeVis, setTimeVis] = useState(false)
     useEffect(() => {
         var localData = []
         for(var i = 0; i < 10; i++){
@@ -19,50 +19,52 @@ function Choices() {
 
 
     return (
-        <div className='ch-bg'>
-            <h1 className="btn-cover">
-                <div className="btn-sel-goal-cover">
-                    <button className='btn-sel-goal btn'>
-                        SELECT GOALS
-                        <img src={arrow} className='btn-img'/>
-                    </button>
-                </div>
-                <div className="btn-sel-times-cover">
-                    <button className='btn-sel-times btn'>
-                        SELECT TIMESLOT
-                        <img src={arrow} className='btn-img'/>
-                    </button>
-                </div>
-            </h1>
-            <div className="box-cover">
-                <h1 className="box-left">
-                    <div className="box-left-text">COURSES</div>
-                    <div className="box-left-courses">
-                        {
-                            data.map((item, index) => (
-                                <div className="courses-item" key={index} id={index}>
-                                    <div className="courses-name" id={index}>{item.name}</div>
-                                    <div className="courses-cg" id={index}>{item.cg + " CGPA"}</div>
-                                    <input type='radio' id={index} value={index} name="course-sel" className='course-sel'/>
-                                </div>
-                            ))
-                        }
+        <div className='ch-area'>
+            <div className='ch-bg'>
+                <h1 className="btn-cover">
+                    <div className="btn-sel-goal-cover">
+                        <button className='btn-sel-goal btn' onClick={() => setGoalVis(!goalVis)}>
+                            SELECT GOALS
+                            <img src={arrow} className='btn-img'/>
+                        </button>
+                    </div>
+                    <div className="btn-sel-times-cover">
+                        <button className='btn-sel-times btn' onClick={() => setTimeVis(!timeVis)}>
+                            SELECT TIMESLOT
+                            <img src={arrow} className='btn-img'/>
+                        </button>
                     </div>
                 </h1>
-                <div className="box-right">
-                    <h1 className="box-right-text">STATS</h1>
-                    <div className="box-right-graphs">
-                        The chart is present here
+                <div className="box-cover">
+                    <h1 className="box-left">
+                        <div className="box-left-text">COURSES</div>
+                        <div className="box-left-courses">
+                            {
+                                data.map((item, index) => (
+                                    <div className="courses-item" key={index} id={index}>
+                                        <div className="courses-name" id={index}>{item.name}</div>
+                                        <div className="courses-cg" id={index}>{item.cg + " CGPA"}</div>
+                                        <input type='radio' id={index} value={index} name="course-sel" className='course-sel'/>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </h1>
+                    <div className="box-right">
+                        <h1 className="box-right-text">STATS</h1>
+                        <div className="box-right-graphs">
+                            The chart is present here
+                        </div>
                     </div>
                 </div>
+                <h1 className="save-btn-cover">
+                    <button className='save-btn'>
+                        <span className='save-btn-span'>
+                            SAVE CHANGES
+                        </span>
+                    </button>
+                </h1>
             </div>
-            <h1 className="save-btn-cover">
-                <button className='save-btn'>
-                    <span className='save-btn-span'>
-                        SAVE CHANGES
-                    </span>
-                </button>
-            </h1>
             <div hidden={!goalVis}>
                 <GoalSel />
             </div>
