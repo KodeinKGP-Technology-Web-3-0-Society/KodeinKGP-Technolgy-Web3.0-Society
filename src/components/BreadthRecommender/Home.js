45cf6ecf9686e45cc75fa29e4c18a7e2b5c5f5b2
Sudhanshumahar7 <154698864+Sudhanshumahar7@users.noreply.github.com>
2024-06-13 12:45:38 +0530

    improvement in bg-image and toast is added

--- src/components/BreadthRecommender/Home.js
+++ src/components/BreadthRecommender/Home.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './home.css';
 import Navbar from '../LandingPage/Navbar';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import myImage from './bgpng.png'; 
 
 const Home = () =>{
-  
+  // useEffect(() => {
+  //   background();
+  //   window.addEventListener('resize', background);
+  //   return () => window.removeEventListener('resize', background);
+  // }, []);
+
+  const notify = (event) => {
+    event.preventDefault();
+    let val = document.getElementById("impufy").value;
+    let pattern = /^[0-9]{2}[A-Z]{2}[0-9]{5}$/;
+    let result = pattern.test(val);
+    if (result) {
+      toast.success('Success', {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+    } else {
+      toast.error('Error', {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+    }
+  };
+            // const background = () => {
+            //   if (window.matchMedia('(max-width: 767px) and (max-height: 1000px)').matches) {
+            //     document.querySelector('.bgimg').src = './bg2png.png';
+            //   } else {
+            //     document.querySelector('.bgimg').src = './bgpng.png';
+            //   }
+            // };
     return (
     <div className="Roll_number_page">
-      <Navbar/>
+      <img src={myImage} className='bgimg' />
+      {/* <Navbar/> */}
       <div className="welcome-card">
         <div className='formForAskingRollNumber'>
         <span className='welcomeToPage'>WELCOME</span>
@@ -19,23 +64,25 @@ const Home = () =>{
             id='impufy'
           />
           <br/>
-          <button type="submit" class="submissionbuttonRollNumber" onClick={()=>{
-              let val =document.getElementById("impufy").value;
-              let pattern=/^[0-9]{2}[A-Z]{2}[0-9]{5}$/;;
-              let result=pattern.test(val)
-              if(result){
-                window.alert("Success");
-              }
-              else{
-                window.alert("error");
-              }
-            }}>SUBMIT</button>
-        </form>
+          <button type="submit" class="submissionbuttonRollNumber" onClick={notify}
+            >SUBMIT</button>
+            <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+              />
+        </form> 
           </div>
       </div>
-    </div>
+     </div>
   );
 }
 
 export default Home;
-
