4bbc2f35141fdd26fb7719fe09560f6a27857eb6
Bikram Ghuku <bikramghuku05@gmail.com>
2024-06-13 11:40:36 +0530

    feat: added dummy goals for select goals

--- src/components/BreadthRecommender/Choices/GoalSel.js
+++ src/components/BreadthRecommender/Choices/GoalSel.js
@@ -2,13 +2,40 @@ import React, { useState } from 'react'
 import './GoalSel.css'
 
 function GoalSel() {
-    const [availTimes ,setAvailTimes] = useState([])
+    const [availGoals ,setAvailGoals] = useState([])
+    useState(() => {
+        setAvailGoals([
+            "Civil Engineer",
+            "Mechanical Engineer",
+            "Electrical Engineer",
+            "Chemical Engineer",
+            "Software Engineer",
+            "Aerospace Engineer",
+            "Environmental Engineer",
+            "Biomedical Engineer",
+            "Industrial Engineer",
+            "Petroleum Engineer",
+            "Structural Engineer",
+            "Computer Engineer",
+            "Robotics Engineer",
+            "Nuclear Engineer",
+            "Automotive Engineer"
+        ])
+    }, [])
     return (
         <div className='gs-main'>
             <div className="gs-opts">
                 <h1 className="gs-opts-title">SELECT GOALS</h1>
             </div>
-            <div className="gs-contents"></div>
+            <div className="gs-contents">
+            {
+                    availGoals.map((data, index) => (
+                        <div className='gs-ele' key={index}>
+                            <div className="gs-ele-time">{data}</div>
+                        </div>
+                    ))
+                }
+            </div>
             <div className="gs-btn">
                 <div className="gs-cl-btn-wrap">
                     <button className='gs-cl-btn'>
