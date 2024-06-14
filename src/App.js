8516f833e654e0fae05f808ef8911583ef830036
shauryaraj-kgp <146989609+shauryaraj-kgp@users.noreply.github.com>
2024-06-14 13:18:35 +0530

    updated App.js to put Timetable

--- src/App.js
+++ src/App.js
@@ -10,31 +10,33 @@ import TheoryQuestion from './components/QnAPage/TheoryProbs/TheoryQuestion';
 
 import TheoryTopic from './components/QnAPage/TheoryProbs/TheoryTopic';
 import TeamsPage from "./components/TeamsPage/TeamsPage";
-import EventsPage  from './components/EventsPage/EventsPage';
+import EventsPage from './components/EventsPage/EventsPage';
 import Home from "./components/LandingPage/Home";
 import ApplyPage from './components/EventsPage/ApplyPage';
 import BreadthRecommender from './components/BreadthRecommender/Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import Timetable from './components/BreadthRecommender/Timetable';
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/pds' element={<Labtheory/>} />
-        <Route path='/pds/lab' element={<Lab/>} />
-        <Route path='/pds/theory' element={<Theory/>} />
-        <Route path='/pds/lab/:topic' element={<LabTopic/>} />
-        <Route path='/pds/theory/:topic' element={<TheoryTopic/>} />
-        <Route path='/pds/lab/:topic/:ind' element={<LabQuestion/>} />
-        <Route path='/pds/theory/:topic/:ind' element={<TheoryQuestion/>} />
-        <Route path='/teams' element={<TeamsPage/>}></Route>
-        <Route path='/events' element={<EventsPage/>}></Route>
-        <Route path='/events/apply' element={<ApplyPage/>} ></Route>
-        <Route path='/breadth-recommend' element={<BreadthRecommender/>} ></Route>
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/pds' element={<Labtheory />} />
+          <Route path='/pds/lab' element={<Lab />} />
+          <Route path='/pds/theory' element={<Theory />} />
+          <Route path='/pds/lab/:topic' element={<LabTopic />} />
+          <Route path='/pds/theory/:topic' element={<TheoryTopic />} />
+          <Route path='/pds/lab/:topic/:ind' element={<LabQuestion />} />
+          <Route path='/pds/theory/:topic/:ind' element={<TheoryQuestion />} />
+          <Route path='/teams' element={<TeamsPage />}></Route>
+          <Route path='/events' element={<EventsPage />}></Route>
+          <Route path='/events/apply' element={<ApplyPage />} ></Route>
+          <Route path='/breadth-recommend' element={<BreadthRecommender />} ></Route>
+          <Route path='/breadth-recommend/timetable' element={<Timetable />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
