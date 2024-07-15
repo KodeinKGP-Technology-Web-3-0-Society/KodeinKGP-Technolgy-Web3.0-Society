9514a5b68d79733cb454cf4b8cd81ef4a70d9f66
Golla Meghanandh Manvith Prabhash <prabhashgolla2004@gmail.com>
2024-07-16 00:03:09 +0530

    Selection Page

--- src/components/TeamsPage/Teams.js
+++ src/components/TeamsPage/Teams.js
@@ -14,10 +14,10 @@ const Teams = () =>{
         <div className='teams'>
         <div className='classif-buttons'>
             <button className={`team-button ${activeButton === '1' ? 'active' : ''}`} id='1' onClick={ButtonClicked}>Tech Team</button>
-            <button className={`team-button ${activeButton === '2' ? 'active' : ''}`} id='2' onClick={ButtonClicked}>AI&Metaverse Team</button>
+            <button className={`team-button ${activeButton === '2' ? 'active' : ''}`} id='2' onClick={ButtonClicked}>AI & Metaverse Team</button>
             <button className={`team-button ${activeButton === '3' ? 'active' : ''}`} id='3' onClick={ButtonClicked}>Blockchain Team</button>
-            <button className={`team-button ${activeButton === '4' ? 'active' : ''}`} id='4' onClick={ButtonClicked}>Design&Media Team</button>
-            <button className={`team-button ${activeButton === '5' ? 'active' : ''}`} id='5' onClick={ButtonClicked}>Events Team</button>
+            <button className={`team-button ${activeButton === '4' ? 'active' : ''}`} id='4' onClick={ButtonClicked}>Events Team</button>
+            <button className={`team-button ${activeButton === '5' ? 'active' : ''}`} id='5' onClick={ButtonClicked}>Design & Media Team</button>
         </div>
         <div>
             <SelectedTeam id={activeButton} />
