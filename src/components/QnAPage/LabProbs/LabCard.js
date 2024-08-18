0ffd8f25d254397115c2a6f3a16b12233600f652
Bikram Ghuku <bikramghuku05@gmail.com>
2024-08-18 17:47:08 +0530

    feat: fix qna page

--- src/components/QnAPage/LabProbs/LabCard.js
+++ src/components/QnAPage/LabProbs/LabCard.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./LabCard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faNewspaper } from "@fortawesome/free-solid-svg-icons";
 
 export default function LabCard({ qna, topic, ind }) {
   let path = `/pds/lab/${topic}/${ind}`;
   return (
     <div className="labCards">
       <tr className="cardDiv">
-        <th className="index">{ind + 1}</th>{" "}
-        <th>
-          <Link to={path} className="quest">
+        <td className="index">{ind + 1}</td>{" "}
+        <td className="lc-ques">
             {qna.Question}
+        </td>
+        <td className="lc-link">
+          <Link to={path} className="quest">
+            <FontAwesomeIcon icon={faNewspaper} />
           </Link>
-        </th>
+        </td>
       </tr>
     </div>
   );
