3ce49ba461bda44d4cf3d17eb23d9fd60eb89883
Saksham <sakshamkumar1415@gmail.com>
2025-06-04 22:32:30 +0530

    Fix: Loading animation removed

--- src/components/LandingPage/Home.jsx
+++ src/components/LandingPage/Home.jsx
@@ -5,8 +5,8 @@ import Landing from './Landing.jsx'
 import './Home.css'
 
 const MainContent = () => {
-	const [animate, setAnimate] = useState(true)
-	if (animate) return <Animation animate={animate} setAnimate={setAnimate} />
+	// const [animate, setAnimate] = useState(true)
+	// if (animate) return <Animation animate={animate} setAnimate={setAnimate} />
 
 	return (
 		<div className="bg-[rgb(1,1,27)] flex flex-col h-full w-full overflow-hidden">
