2f413e70fafa7443e87516b6e34b54780c16f4f0
Naitik Agarwal <naitikagar12@gmail.com>
2025-06-05 19:39:10 +0530

    update Lab.jsx fix:SubTopics

--- src/components/QnAPage/LabProbs/SubTopics.jsx
+++ src/components/QnAPage/LabProbs/SubTopics.jsx
@@ -18,13 +18,13 @@ export default function SubTopics({ topic, viewMode }) {
 				{Object.keys(dataJ[topic][0]['Subtopics']).map((subTopic, index) => (
 					<div key={index} className="w-full bg-[#01011b] rounded-lg !mb-4 overflow-hidden">
 						<div
-							className="dropdown-header"
+							className="text-[1.5rem] font-semibold bg-[#43434390] text-white flex justify-between cursor-pointer border-b border-b-[#5be6ff] items-center !p-5"
 							onClick={() => toggleSubTopic(subTopic)}
 						>
 							{subTopic
 								.replace(/([a-z])([A-Z0-9])|([A-Z])([0-9])/g, '$1$3 $2$4')
 								.replace(/\bAnd\b/g, 'and')}
-							<span className="dropdown-icon">
+							<span className="text-[1.5rem]">
 								{openSubTopics.includes(subTopic) ? '-' : '+'}
 							</span>
 						</div>
