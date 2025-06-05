7a0424f8d12ec40d745175e65c3fa8386350f476
Aashrith Vellampalli <aashrith@Aashriths-MacBook-Pro.local>
2025-06-05 19:45:37 +0530

    Teams Page Migration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // make sure Tailwind scans your files
   theme: {
     extend: {
-      animation: {
-        shine: 'shine 1.5s ease-in-out infinite',
-        'pulse-slow': 'pulse-slow 3s ease-in-out infinite',
+        screens: {
+        'max-lg': {'max': '1000px'},
+        'max-md': {'max': '900px'},
       },
-      keyframes: {
-        shine: {
-          '0%': { transform: 'translateX(-100%) rotate(-35deg)', opacity: '0' },
-          '10%': { opacity: '0.5' },
-          '100%': { transform: 'translateX(100%) rotate(-35deg)', opacity: '0' }
-        },
-        'pulse-slow': {
-          '0%, 100%': { opacity: '0' },
-          '50%': { opacity: '1' }
-        }
-      }
+      backgroundImage: {
+        'fancy-radial': 'radial-gradient(64.18% 64.18% at 71.16% 35.69%, #def9fa 0.89%, #bef3f5 17.23%, #9dedf0 42.04%, #7de7eb 55.12%, #5ce1e6 71.54%, #33bbcf 100%)',
+      },
+      fontFamily: {
+        kanit: ['Kanit', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
     },
   },
   plugins: [],
-} 
+};
